071c5f102f33f08859d70a254f7025c35c51ddb5
Surya Neupane <surya.neu71@gmail.com>
2017-02-01 22:33:25 +0545

    Deleted unwanted code of line 10

--- src/TestClass.java
+++ src/TestClass.java
@@ -7,7 +7,7 @@ public class TestClass {
 	public static void main(String[] args)
 	{
 		MessagePrint m1 = new MessagePrint();
-		System.out.println(m1.toMilitary());
+		//System.out.println(m1.toMilitary());
 		m1.setTime(13,12,3);
 		System.out.println(m1.toMilitary());
 		System.out.println(m1.toString());
